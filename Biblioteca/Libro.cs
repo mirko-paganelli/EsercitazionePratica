514876131010c68c08a0ce66de9a0369e87c0944
Mirko Paganelli <MIRKO.PAGANELLI@local.ittcesena.it>
2024-10-21 09:15:34 +0200

    Release 0.1.0: Creazione della classe Libro e implementazione della funzione toString()

---
+++ Biblioteca/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Libro(string autore,string titolo, int annoPubblicazione, string editore, int numeroPagine)
+    {
+        string autore = autore;
+        string titolo = titolo; 
+        int annoPubblicazione = annoPubblicazione;
+        string editore = editore;
+        int numeroPagine = numeroPagine;
+
+        public string toString()
+        {
+            string stringa;
+            return stringa = $"{autore}, {titolo}, {annoPubblicazione}, {editore}, {numeroPagine}";
+        }
+    }
+}
