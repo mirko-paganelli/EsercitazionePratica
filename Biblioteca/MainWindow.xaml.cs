bef2e1d4f62f4d92b282017623e59f6d85238c48
Mirko Paganelli <MIRKO.PAGANELLI@local.ittcesena.it>
2024-10-21 08:53:44 +0200

    Release 0: Creazione dell'interfaccia sul master

---
+++ Biblioteca/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteca
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AggiungiLibro_Click(object sender, RoutedEventArgs e)
+        {
+            string autore, titolo, editore;
+            int annoPubblicazione, numeroPagine;
+        }
+
+        private void btn_CercaLibro_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CercaLibroAutore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_NumeroLibri_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
