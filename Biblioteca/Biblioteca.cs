d100113df37c196268d6c78215e885eb6a5738c8
Mirko Paganelli <MIRKO.PAGANELLI@local.ittcesena.it>
2024-10-21 09:46:39 +0200

    Release 1.1.0: creazione delle funzioni CercaLibroTitolo() e CercaLibriAutore() e parziale implementazione

--- Biblioteca/Biblioteca.cs
+++ Biblioteca/Biblioteca.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Biblioteca
 {
-    internal class Biblioteca
+    internal class Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura, List<Libro> elencoLibri)
     {
+        string nome = nome;
+        string indirizzo = indirizzo;
+        string orarioApertura = orarioApertura;
+        string orarioChiusura = orarioChiusura;
+        List <Libro> elencoLibri = elencoLibri;
+
+        public bool CercaLibroTitolo() 
+        {
+            return false;//TODO
+        }
+        public bool CercaLibriAutore()
+        {
+            return false;//TODO
+        }
+
+
     }
 }
